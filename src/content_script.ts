f9f2566e5128377b717b2988f275c0d2e506e196
Abestanis <abestanis.gc@gmail.com>
2021-01-24 02:48:47 +0100

    Add an option to re-detect password fields when the website changes

--- src/content_script.ts
+++ src/content_script.ts
@@ -5,4 +5,20 @@ const pageControl = new PageControl();
 
 $(()=>{
     pageControl.detectFields();
+    if (pageControl.settings.searchForInputsOnUpdate) {
+        const observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                for (let node of mutation.addedNodes) {
+                    let passwordFields = $(node).find('input[type="password"]');
+                    if (passwordFields.length) {
+                        setTimeout(() => pageControl.detectNewFields(passwordFields), 100);
+                    }
+                }
+            });
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    }
 });
